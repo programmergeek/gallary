efdceb7622cae2c8666660ea89dba9ef4363c912
Will <programergeek@gmail.com>
2023-04-01 20:26:55 +0200

    style: create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Container } from "@mui/material";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div id="container">
+      <div id="nav"></div>
+      <div id="image"></div>
+      <div id="image-list"></div>
+      <div id="text">
+        <p id="title"></p>
+        <p id="description"></p>
+        <div id="social-links"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
