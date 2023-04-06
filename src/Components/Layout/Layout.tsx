2ceafce6230222296850261c135c16834c528fd6
Will <programergeek@gmail.com>
2023-04-06 22:19:01 +0200

    feat: define layout sub-components

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -1,9 +1,42 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import { ImageList } from "@mui/material";
 
 type Props = {
   children: React.ReactNode;
 };
 
-export const Layout: React.FC<Props> = ({ ...props }) => {
-  return <div>{props.children}</div>;
+type LayoutSubComponents = {
+  Navbar: typeof Navbar;
+  Text: typeof TextBlock;
+  Image: typeof FocusImage;
+  PreviewImages: typeof PreviewImages;
 };
+
+export const Layout: React.FC<Props> & LayoutSubComponents = (props: Props) => {
+  return <Box>{props.children}</Box>;
+};
+
+const Navbar = (props: Props) => {
+  return <div> {props.children} </div>;
+};
+
+Layout.Navbar = Navbar;
+
+const TextBlock = (props: Props) => {
+  return <div></div>;
+};
+
+Layout.Text = TextBlock;
+
+const FocusImage = (props: Props) => {
+  return <div></div>;
+};
+
+Layout.Image = FocusImage;
+
+const PreviewImages = (props: Props) => {
+  return <div></div>;
+};
+
+Layout.PreviewImages = PreviewImages;
